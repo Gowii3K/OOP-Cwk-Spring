6be0227db0ee8cd46b6afedb93a882c8d572fe04
Gowtham Raveeindran <rgadithya997@gmail.com>
2024-11-03 11:20:08 +0530

    Switched to basic sync methods

--- src/main/java/com/oop/cwk/TicketPool.java
+++ src/main/java/com/oop/cwk/TicketPool.java
@@ -1,21 +1,16 @@
 package com.oop.cwk;
-import java.io.IOException;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.locks.Condition;
+
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
-import java.util.logging.FileHandler;
-import java.util.logging.SimpleFormatter;
 
 
 public class TicketPool {
 
     static Logger logger=Logger.getLogger(TicketPool.class.getName());
-    private final Lock lock = new ReentrantLock();
-    private final Condition notFull = lock.newCondition();
-    private final Condition notEmpty = lock.newCondition();
-    private final Condition pausedCondition = lock.newCondition();
+
     int totalTickets;
     int maximumTicketCapacity;
     ConcurrentLinkedQueue<Integer> availableTickets = new ConcurrentLinkedQueue<>();
@@ -36,15 +31,10 @@ public class TicketPool {
         isPaused = true;
         System.out.println("TicketPool paused.");
     }
-    public void resume() {
-            lock.lock(); // Acquire the lock
-            try {
-                isPaused = false; // Update the paused state
-                pausedCondition.signalAll(); // Notify any waiting threads
-                System.out.println("TicketPool resumed.");
-            } finally {
-                lock.unlock(); // Ensure the lock is released
-            }
+    public synchronized void resume() {
+            isPaused = false;
+            notifyAll();
+        System.out.println("TicketPool resumed.");
         }
 
 
@@ -52,17 +42,16 @@ public class TicketPool {
 
     private void checkPaused() throws InterruptedException {
 
-            while (isPaused) {
-                pausedCondition.await(); // Wait until signaled to continue
-            }
-        // Ensure the lock is released
+        while (isPaused) {
+            System.out.println("checking paused");
+            wait();
+        }
 
     }
 
 
-    public  void addTickets(int TicketsPerRelease, int releaseInterval,int vendorId ,Vendor vendor) throws InterruptedException {
-        lock.lock();
-        try {
+    public  synchronized void addTickets(int TicketsPerRelease, int releaseInterval,int vendorId ,Vendor vendor) throws InterruptedException {
+
             while (true) {
                 checkPaused();
                 //checks max capacity and notifies waiting threads,so they can terminate
@@ -73,7 +62,7 @@ public class TicketPool {
                 //checks if it should wait for customers to buy
                 else if (availableTickets.size() == maximumTicketCapacity) {
                     System.out.println("wait for customers to buy");
-                    notFull.await();
+                    wait();
                 }
                 // if neither of the above condition is satisfied proceed with adding tickets
                 else {
@@ -98,23 +87,21 @@ public class TicketPool {
                     //notify waiting threads
 
                     Thread.sleep(releaseInterval);
-                    notEmpty.signalAll();
+                    notifyAll();
                     break;
                 }
             }
-        }
-        finally {
-            lock.unlock();
-        }
+
+
 
     }
 
 
-    public  void removeTicket(int retrievalInterval,int customerId, Customer customer) throws InterruptedException {
+    public  synchronized void removeTicket(int retrievalInterval,int customerId, Customer customer) throws InterruptedException {
 
         Integer purchasedTicket;
-        lock.lock();
-        try {
+
+
             while (true) {
                 checkPaused();
                 //total tickets out and no more on sale
@@ -125,21 +112,19 @@ public class TicketPool {
                 //nothing on sale but it will be put up eventually
                 else if (totalTickets > 0 && availableTickets.isEmpty()) {
                     System.out.println("wait for vendors to add pls");
-                    notEmpty.await();
+                    wait();
 
                 } else {
                     purchasedTicket = availableTickets.poll();
                     customer.ticketBought(purchasedTicket);
                     System.out.println("Ticket No " + purchasedTicket + "of event  " + "bought by " + customerId);
                     Thread.sleep(retrievalInterval);
-                    notFull.signalAll();
+                    notifyAll();
                     break;
                 }
             }
         }
-        finally {
-            lock.unlock();
-        }
+
 
     }
-}
+
